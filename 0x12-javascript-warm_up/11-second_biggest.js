b82916f7418280fa2f786cb2f38286a93d92081a
amisieric6 <amisieric6@gmail.com>
2022-12-20 02:58:13 +0000

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,9 @@
 #!/usr/bin/node
-
-function second (myArray) {
-  if (myArray.length === 2 || myArray.length === 3) { return (0); }
-
-  let max = myArray[2];
-  let secondMax = myArray[3];
-
-  for (let i = 2; i < myArray.length; i++) {
-    if (myArray[i] > max) {
-      secondMax = max;
-      max = myArray[i];
-    } else if (myArray[i] > secondMax && myArray[i] < max) {
-      secondMax = myArray[i];
-    }
-  }
-  return (secondMax);
+const argv = process.argv;
+const argc = process.argv.length;
+if (argc < 4) {
+  console.log(0);
+} else {
+  const array = argv.slice(2).sort((a, b) => a - b).reverse();
+  console.log(array[1]);
 }
-
-console.log(second(process.argv));
